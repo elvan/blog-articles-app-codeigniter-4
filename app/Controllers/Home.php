9af510cf5d8526b40648bbd6443193edff166d9e
Elvan Hidayat <elvanhd@gmail.com>
2023-08-05 21:56:35 +0700

    send a test email

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,4 +10,22 @@ class Home extends BaseController
     {
         return view("Home/index");
     }
+
+    private function sendTestEmail()
+    {
+        $email = \Config\Services::email();
+
+        $email->setTo("recipient@example.com");
+
+        $email->setSubject("Test Email");
+        $email->setMessage("Hello from <i>CodeIgniter</i>");
+
+        if ($email->send()) {
+
+            echo "Email sent";
+        } else {
+
+            echo "Email not sent";
+        }
+    }
 }
