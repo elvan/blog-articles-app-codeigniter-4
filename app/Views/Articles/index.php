1be1e4ee86354c42e9fee488ce450cdc3a3c9f3d
Elvan Hidayat <elvanhd@gmail.com>
2023-08-05 07:40:14 +0700

    include one view in another

--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
+<?= $this->include("header") ?>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Articles</title>
-</head>
-
-<body>
-
-    <h1>Articles</h1>
+<h1>Articles</h1>
 
 </body>
 
