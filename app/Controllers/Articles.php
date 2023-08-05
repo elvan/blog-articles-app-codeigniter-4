a753bf5ec3397c449a1cc5d38e136425e647bb81
Elvan Hidayat <elvanhd@gmail.com>
2023-08-05 08:05:55 +0700

    create the article migration

--- app/Controllers/Articles.php
+++ app/Controllers/Articles.php
@@ -8,10 +8,6 @@ class Articles extends BaseController
 {
     public function index()
     {
-        $db = db_connect();
-
-        dd($db->listTables());
-
         $data = [
             ["title" => "One", "content" => "The first"],
             ["title" => "Two", "content" => "Some content"]
