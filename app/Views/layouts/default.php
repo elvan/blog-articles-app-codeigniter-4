a502c8b12319e6418adad52c3f0cad8f02be0443
Elvan Hidayat <elvanhd@gmail.com>
2023-08-05 09:13:40 +0700

    add Water CSS classless css

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $this->renderSection("title") ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 
 <body>
