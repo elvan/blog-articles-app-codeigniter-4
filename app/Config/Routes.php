3a3797f1a324ccb53d9bfca437cc181db9b81398
Elvan Hidayat <elvanhd@gmail.com>
2023-08-06 18:42:49 +0700

    validate the new password

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -44,6 +44,7 @@ $routes->resource("articles", ["placeholder" => "(:num)"]);
 service('auth')->routes($routes);
 
 $routes->get("set-password", "Password::set");
+$routes->post("set-password", "Password::update");
 
 /*
  * --------------------------------------------------------------------
--- app/Controllers/Password.php
+++ app/Controllers/Password.php
@@ -10,4 +10,26 @@ class Password extends BaseController
     {
         return view("Password/set");
     }
+
+    public function update()
+    {
+        $rules = [
+            "password" => [
+                "label" => "Password",
+                "rules" => "required|strong_password"
+            ],
+            "password_confirmation" => [
+                "label" => "Password Confirmation",
+                "rules" => "required|matches[password]"
+            ]
+        ];
+
+        if (!$this->validate($rules)) {
+
+            return redirect()->back()
+                ->with("errors", $this->validator->getErrors());
+        }
+
+        echo "Valid";
+    }
 }
