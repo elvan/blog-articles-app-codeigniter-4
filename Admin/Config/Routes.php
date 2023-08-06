b449c6b555154c9c0577ca60d0cc3bcd54a1e1b1
Elvan Hidayat <elvanhd@gmail.com>
2023-08-06 19:16:21 +0700

    add a routes config to the admin module

---
+++ Admin/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Admin\Config;
+
+use Config\Services;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+$routes->get("admin/users", "\Admin\Controllers\Users::index");
