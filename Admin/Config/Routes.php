9c962cacd55e897705c82950e7265ab63d4f3a66
Elvan Hidayat <elvanhd@gmail.com>
2023-08-06 21:50:18 +0700

    routes grouping

--- Admin/Config/Routes.php
+++ Admin/Config/Routes.php
@@ -7,5 +7,7 @@ use Config\Services;
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
-$routes->get("admin/users", "\Admin\Controllers\Users::index");
-$routes->get("admin/users/(:num)", "\Admin\Controllers\Users::show/$1");
+$routes->group("admin", ["namespace" => "Admin\Controllers"], static function ($routes) {
+  $routes->get("users", "Users::index");
+  $routes->get("users/(:num)", "Users::show/$1");
+});
