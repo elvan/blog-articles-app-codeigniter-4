14578a45dd1eea8a7a04e5d945a2161469a4905b
Elvan Hidayat <elvanhd@gmail.com>
2023-08-07 08:04:13 +0700

    using the shield session filter

--- Admin/Config/Routes.php
+++ Admin/Config/Routes.php
@@ -7,7 +7,7 @@ use Config\Services;
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
-$routes->group("admin", ["namespace" => "Admin\Controllers"], static function ($routes) {
+$routes->group("admin", ["namespace" => "Admin\Controllers", "filter" => "session"], static function ($routes) {
   $routes->get("users", "Users::index");
   $routes->get("users/(:num)", "Users::show/$1");
   $routes->post("users/(:num)/toggle-ban", "Users::toggleBan/$1");
